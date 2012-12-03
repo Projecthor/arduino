27742d13f0876a76e8eae29591aa7c5c767df599
lucas8 <luc.linux@mailoo.org>
2012-12-03 16:33:42 +0100

    Début de la classe mot.

--- mot.cpp
+++ mot.cpp
@@ -1,3 +1,51 @@
 #include "mot.hpp"
 
+Motor::Motor()
+	: m_angle(0), m_dest(0)
+{
+	// TODO initialisation du moteur
+}
+
+Motor::~Motor()
+{
+	// TODO libération du moteur
+}
+
+int Motor::currentAngle() const
+{
+	return m_angle;
+}
+
+int Motor::destAngle() const
+{
+	return m_dest;
+}
+
+Motor& Motor::operator=(int angle)
+{
+	if(angle >= 45)
+		m_dest = 45;
+	else if(angle <= 0)
+		m_dest = 0;
+	else
+		m_dest = angle;
+	return *this;
+}
+
+Motor& Motor::operator+=(int angle)
+{
+	angle = m_dest + angle;
+	return (*this = angle);
+}
+
+Motor& Motor::operator-=(int angle)
+{
+	angle = m_dest - angle;
+	return (*this = angle);
+}
+
+void Motor::update()
+{
+	// Rotation du moteur
+}
 
--- mot.hpp
+++ mot.hpp
@@ -1,12 +1,24 @@
 
-#ifndef DEF_MOT
-#define DEF_MOT
+#ifndef DEF_MOTOR
+#define DEF_MOTOR
 
-class Mot
+class Motor
 {
 	public:
-	private:
+		Motor();
+		~Motor();
+
+		int currentAngle() const;
+		int destAngle() const;
+		Motor& operator=(int angle);
+		Motor& operator+=(int angle);
+		Motor& operator-=(int angle);
+		void update();
+
+	private: 
+		int m_angle; // Angle actuel en degrés
+		int m_dest; // Angle à obtenir en gegrés
 };
 
-#endif//DEF_MOT
+#endif//DEF_MOTOR
 
