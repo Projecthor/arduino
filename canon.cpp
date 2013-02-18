ebc095cb43279c35e781875a0ae73fe9cd667196
lucas8 <luc.linux@mailoo.org>
2013-02-18 14:51:45 +0100

    Ajout de print dans canon pour le débug.

--- canon.cpp
+++ canon.cpp
@@ -1,18 +1,23 @@
 #include "canon.hpp"
+#include "Arduino.h"
 
 Canon::Canon()
 {
+	Serial.begin(9600);
+	Serial.println("Canon !");
 	// TODO initialiser canon
 }
 
 void Canon::load()
 {
 	// TODO faire charger le canon
+	Serial.println("Chargement du canon.");
 }
 
 void Canon::fire()
 {
 	// TODO déclencher le tir
+	Serial.println("FIRE !!!");
 }
 
 
