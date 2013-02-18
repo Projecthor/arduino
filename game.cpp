74d033de40ea2c8a7aa0e01973fe96540d6ab98b
lucas8 <luc.linux@mailoo.org>
2013-02-18 14:48:05 +0100

    Ajout de la gestion du score dans game.

--- game.cpp
+++ game.cpp
@@ -4,7 +4,7 @@
 const int distPin = 14;
 
 	Game::Game(Bluetooth* connection)
-: m_con(connection), m_dif(NONE), m_dist(0)
+: m_con(connection), m_dif(NONE), m_dist(0), m_score(0)
 {
 	pinMode(distPin, OUTPUT);
 	randomSeed( analogRead(0) ); // Initialisation des valeurs aléatoires
@@ -14,31 +14,33 @@ bool Game::waitForDifficulty()
 {
 	unsigned int first = millis();
 	char buffer[1];
+begin:
 	while( m_con->receive(buffer, 1) != 1 )
 	{
-		switch( buffer[0] )
-		{
-			case 0:
-				m_dif = EASY;
-				break;
-			case 1:
-				m_dif = NORMAL;
-				break;
-			case 2:
-				m_dif = DIFFICULT;
-				break;
-			case 3:
-				m_dif = DIVINITY;
-				break;
-			default:
-				buffer[0] = 0xff;
-				return false;
-				break;
-		}
-
-		if( millis() - first > m_con->toWait )
-			return false;
+		// if( millis() - first > m_con->toWait )
+		//	return false;
 	}
+
+	buffer[0] -= '0';
+	switch( buffer[0] )
+	{
+		case 0:
+			m_dif = EASY;
+			break;
+		case 1:
+			m_dif = NORMAL;
+			break;
+		case 2:
+			m_dif = DIFFICULT;
+			break;
+		case 3:
+			m_dif = DIVINITY;
+			break;
+		default:
+			goto begin;
+			break;
+	}
+
 	return true;
 }
 
@@ -47,21 +49,43 @@ void Game::getDistance()
 	m_dist = computeDistance();
 }
 
-bool Game::waitOrder()
+bool Game::waitComputeOrder()
 {
 	unsigned int first = millis();
 	char buffer[2];
-	while( m_con->receive(buffer, 2) != 2 )
+beginwait:
+	while( m_con->receive(buffer, 2) != 1 )
 	{
-		if( buffer[0] != 0x0f )
-			return false;
-		buffer[0] = 0xf0;
-
-		if( millis() - first > m_con->toWait )
-			return false;
+		// if( millis() - first > m_con->toWait )
+		//	return false;
 	}
+
+	if( buffer[0] == 'e' )
+		return false;
+	else if( buffer[0] != 'c' )
+		goto beginwait;
+
 	return true;
 }
+bool Game::waitFireOrder()
+{
+	unsigned int first = millis();
+	char buffer[2];
+beginfire:
+	while( m_con->receive(buffer, 2) != 1 )
+	{
+		// if( millis() - first > m_con->toWait )
+		//	return false;
+	}
+
+	if( buffer[0] == 'e' )
+		return false;
+	else if( buffer[0] != 'f' )
+		goto beginfire;
+
+	return true;
+}
+
 
 int Game::computeAngle()
 {
@@ -96,6 +120,27 @@ int Game::computeAngle()
 			dist = 0;
 			break;
 	}
+
+	// Le score fait
+	if( dist < 25 )
+		m_score = 9;
+	else if( dist < 50 )
+		m_score = 8;
+	else if( dist < 75 )
+		m_score = 7;
+	else if( dist < 100 )
+		m_score = 6;
+	else if( dist < 133 )
+		m_score = 5;
+	else if( dist < 166 )
+		m_score = 4;
+	else if( dist < 200 )
+		m_score = 3;
+	else if( dist < 250 )
+		m_score = 2;
+	else
+		m_score = 1;
+
 	if( random(0,2) == 0 )
 		dist *= -1;
 
@@ -171,3 +216,7 @@ int Game::relToAngle(int dist)
 	return angle;
 }
 
+int Game::getScore()
+{
+	return m_score;
+}
--- game.hpp
+++ game.hpp
@@ -14,21 +14,24 @@ class Game
 		bool waitForDifficulty(); // Si false fin du jeu
 		void getDistance();
 
-		bool waitOrder(); // Attend l'ordre de tirer -> si false fin du jeu
+		bool waitComputeOrder(); // Attend l'ordre de charger -> si false fin du jeu
+		bool waitFireOrder(); // Attend l'ordre de tirer -> si false fin du jeu
 		int computeAngle(); // détermine l'angle de tir
+		int getScore();
 		bool checkSecurity(); // Teste si quelqu'un passe devant, renvoie vrai si qqun
 
 	private:
 		Bluetooth* m_con; // connection
 		Difficulty m_dif;
 		unsigned int m_dist; // Distance de la cible en mm
+		int m_score;
 
 		unsigned int computeDistance();
 		int relToAngle(int dist); // convertit une distance verticale par rapport au centre de la cible en angle nécessaire
 		int distFunction(int angle); // lie l'angle à la distance verticale sur la cible
 
 		const unsigned int maxMov = 20; // différence maxi lors de la détection d'obstacle en mm
-		const unsigned int sizeCible = 500; // Hauteur de la cible en mm
+		const unsigned int sizeCible = 600; // Hauteur de la cible en mm
 		const unsigned int midCible = sizeCible / 2; // Milieu de la cible
 		const unsigned int thirdCible = midCible / 3;
 		const unsigned int origVit = 6260; // vitesse à la sortie du canon en mm/s
